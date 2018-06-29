24cc1eb4874f90436bb0c7f51e919b4dc61fa1cf
Brett <chienb@users.noreply.github.com>
2018-06-28 22:58:41 -0700

    update firebase database ref

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,7 +1,18 @@
 (function() {
 	function Message($firebaseArray) {
-		var firebaseRef = new Firebase("https://glaring-inferno-3086.firebaseio.com/");
-		var messages = $firebaseArray(firebaseRef.child('messages'));
+// 		var firebaseRef = new Firebase("https://glaring-inferno-3086.firebaseio.com/");
+		  // Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyBYbsXxdStO5yagrRmTbsylLQATx9Sei3A",
+		    authDomain: "brett-bloc-chat.firebaseapp.com",
+		    databaseURL: "https://brett-bloc-chat.firebaseio.com",
+		    projectId: "brett-bloc-chat",
+		    storageBucket: "brett-bloc-chat.appspot.com",
+		    messagingSenderId: "967177598829"
+		  };
+		  firebase.initializeApp(config);
+		  var rootRef = firebase.database().ref();
+		var messages = $firebaseArray(rootRef.child('messages'));
 		
 		return {
 			all: messages,
